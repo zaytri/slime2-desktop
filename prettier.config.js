ada62be841c76a2ccf13c93ec0fad03a0e0c7d71
Zaytri <9072119+zaytri@users.noreply.github.com>
2024-04-18 21:54:52 -0700

    feat: drag/drop and read a JSON file

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+const config = {
+  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
+  arrowParens: 'avoid',
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  tailwindFunctions: ['clsx'],
+  organizeImportsSkipDestructiveCodeActions: true,
+  tabWidth: 2,
+  useTabs: false
+}
+
+export default config
--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,98 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import { readTextFile } from '@tauri-apps/api/fs';
+import { invoke } from '@tauri-apps/api/tauri';
+import { FileDropEvent, appWindow } from '@tauri-apps/api/window';
+import clsx from 'clsx';
+import { useEffect, useState } from 'react';
+import './App.css';
+import reactLogo from './assets/react.svg';
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [greetMsg, setGreetMsg] = useState('');
+  const [name, setName] = useState('');
+  const [dragAndDropState, setDragAndDropState] =
+    useState<FileDropEvent['type']>('cancel');
+  const [filePath, setFilePath] = useState('');
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+    setGreetMsg(await invoke('greet', { name }));
+  }
+
+  useEffect(() => {
+    let unlisten = () => {};
+
+    async function dragAndDrop() {
+      unlisten = await appWindow.onFileDropEvent(event => {
+        setDragAndDropState(event.payload.type);
+
+        switch (event.payload.type) {
+          case 'hover':
+            {
+            }
+            break;
+          case 'drop':
+            {
+              setFilePath(event.payload.paths[0]);
+            }
+            break;
+          default: {
+          }
+        }
+      });
+    }
+
+    dragAndDrop();
+
+    return () => {
+      unlisten();
+    };
+  });
+
+  if (filePath) {
+    readTextFile(filePath).then(contents => {
+      // JSON.parse can throw an error if not valid JSON, handle that
+      const parse = JSON.parse(contents);
+      console.log(parse);
+    });
   }
 
   return (
-    <div className="container">
+    <div
+      className={clsx(
+        'container',
+        dragAndDropState === 'hover' && 'container-hover',
+        dragAndDropState === 'drop' && 'container-drop',
+      )}
+    >
       <h1>Welcome to Tauri!</h1>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+      <div className='row'>
+        <a href='https://vitejs.dev' target='_blank'>
+          <img src='/vite.svg' className='logo vite' alt='Vite logo' />
         </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+        <a href='https://tauri.app' target='_blank'>
+          <img src='/tauri.svg' className='logo tauri' alt='Tauri logo' />
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href='https://reactjs.org' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
 
       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
       <form
-        className="row"
-        onSubmit={(e) => {
+        className='row'
+        onSubmit={e => {
           e.preventDefault();
           greet();
         }}
       >
         <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
+          id='greet-input'
+          onChange={e => setName(e.currentTarget.value)}
+          placeholder='Enter a name...'
         />
-        <button type="submit">Greet</button>
+        <button type='submit'>Greet</button>
       </form>
 
       <p>{greetMsg}</p>
