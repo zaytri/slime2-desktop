eabd72acf7f93cf1fd6ccfab61407f2f031bc2d0
Zaytri <9072119+zaytri@users.noreply.github.com>
2026-05-12 23:10:44 -0700

    add quick dev peek

--- src/components/widget_settings/SettingsGroup.tsx
+++ src/components/widget_settings/SettingsGroup.tsx
@@ -1,8 +1,11 @@
+import { useSettings } from '@/contexts/settings/useSettings';
 import useWidgetSettingParent from '@/contexts/widget_setting_parent/useWidgetSettingParent';
 import { getWidgetValueChildKey } from '@/contexts/widget_setting_parent/useWidgetValueKey';
 import useWidgetValues from '@/contexts/widget_values/useWidgetValues';
 import { i18nStringTransform, i18nUndefined } from '@/helpers/i18n';
 import type { WidgetSetting as WidgetSettingType } from '@@/json/widgetSettings';
+import EyeSvg from '@@/svg/EyeSvg';
+import { Tooltip, TooltipAnchor, TooltipProvider } from '@ariakit/react';
 import WidgetSetting from './WidgetSetting';
 
 type SettingsGroupProps = {
@@ -15,6 +18,7 @@ type SettingsGroupProps = {
 export default function SettingsGroup({ settings }: SettingsGroupProps) {
 	const values = useWidgetValues();
 	const parentId = useWidgetSettingParent();
+	const { settings: appSettings } = useSettings();
 
 	return (
 		<>
@@ -59,15 +63,52 @@ export default function SettingsGroup({ settings }: SettingsGroupProps) {
 				const alt = 'alt' in setting ? i18nStringTransform(setting.alt) : '';
 
 				return (
-					<WidgetSetting
-						key={id}
-						id={id}
-						{...setting}
-						label={label}
-						placeholder={placeholder}
-						description={description}
-						alt={alt}
-					/>
+					<div className='flex'>
+						{appSettings.devMode && (
+							<TooltipProvider timeout={0} placement='right'>
+								<TooltipAnchor
+									className='z-10 -mb-100 -ml-4 self-start'
+									render={<button type='button' />}
+								>
+									<div className='rounded-1 p-1 text-zinc-600 over:bg-zinc-700 over:text-white over:outline over:outline-zinc-800'>
+										<span className='sr-only'>Dev Peek</span>
+										<EyeSvg className='size-4' />
+									</div>
+								</TooltipAnchor>
+
+								<Tooltip className='dark-menu p-0!'>
+									<p className='dark-menu-item flex justify-center px-2 py-1 font-mono select-all not-only:border-b not-only:border-zinc-500'>
+										{parentId ? getWidgetValueChildKey(parentId, id) : id}
+									</p>
+									{'options' in setting && (
+										<div className='flex flex-col px-2 py-1'>
+											{setting.options.map(option => {
+												return (
+													<div className='dark-menu-item flex gap-1 p-0!'>
+														<p>{i18nStringTransform(option.label)}:</p>
+														<p className='font-mono'>
+															{JSON.stringify(option.value)}
+														</p>
+													</div>
+												);
+											})}
+										</div>
+									)}
+								</Tooltip>
+							</TooltipProvider>
+						)}
+						<div className='flex flex-1 flex-col'>
+							<WidgetSetting
+								key={id}
+								id={id}
+								{...setting}
+								label={label}
+								placeholder={placeholder}
+								description={description}
+								alt={alt}
+							/>
+						</div>
+					</div>
 				);
 			})}
 		</>
