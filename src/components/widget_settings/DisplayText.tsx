84c97a199a57f47dc921c21f1d6c9032bd4cbc35
Zaytri <9072119+zaytri@users.noreply.github.com>
2026-05-26 04:04:35 -0700

    restyle display text again

--- src/components/widget_settings/DisplayText.tsx
+++ src/components/widget_settings/DisplayText.tsx
@@ -6,8 +6,8 @@ type DisplayTextProps = {
 
 export default function DisplayText({ label }: DisplayTextProps) {
 	return (
-		<div className='rounded-2 border border-zinc-800 bg-zinc-700 p-2 font-medium text-white text-shadow-[0_1px_black]'>
-			<LinkifyText linkClassName='text-green-300 font-semibold'>
+		<div className='rounded-2 border-x-8 border-y border-lime-600 bg-lime-200 p-2 font-semibold text-green-950'>
+			<LinkifyText linkClassName='text-green-700 font-semibold'>
 				{label}
 			</LinkifyText>
 		</div>
