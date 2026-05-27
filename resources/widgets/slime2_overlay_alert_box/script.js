fdff4152533c408268321eef5a69371a17cd2948
Zaytri <9072119+zaytri@users.noreply.github.com>
2026-05-26 22:03:31 -0700

    alert box: base code

--- resources/widgets/slime2_overlay_alert_box/script.js
+++ resources/widgets/slime2_overlay_alert_box/script.js
@@ -38,7 +38,6 @@ addEventListener('slime2:twitch-event', twitchEventListener);
 
 function widgetValuesListener(event) {
 	logEventData(event.type, event.detail);
-
 	Widget.values = new Map(Object.entries(event.detail));
 }
 
@@ -76,65 +75,23 @@ function twitchEventListener(event) {
 	// https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types/
 	// event.detail.data = event of each of these EventSub payloads
 	switch (type) {
-		// chat
-		case 'channel.chat.message':
-		case 'channel.chat.message_delete':
-		case 'channel.chat.clear':
-		case 'channel.chat.clear_user_messages':
-		case 'channel.chat.notification':
-
 		// follow
 		case 'channel.follow':
 
 		// raid
 		case 'channel.raid':
 
-		// shoutout
-		case 'channel.shoutout.create':
-
 		// channel points
 		case 'channel.channel_points_custom_reward_redemption.add':
-		case 'channel.channel_points_automatic_reward_redemption.add':
-
-		// hype train
-		case 'channel.hype_train.begin':
-		case 'channel.hype_train.progress':
-		case 'channel.hype_train.end':
 
 		// bits
-		case 'channel.bits.use':
 		case 'channel.cheer':
 		case 'channel.custom_power_up_redemption.add':
-
-		// ad break
-		case 'channel.ad_break.begin':
 
 		// subscriptions
 		case 'channel.subscribe':
 		case 'channel.subscription.gift':
 		case 'channel.subscription.message':
-
-		// polls
-		case 'channel.poll.begin':
-		case 'channel.poll.progress':
-		case 'channel.poll.end':
-
-		// predictions
-		case 'channel.prediction.begin':
-		case 'channel.prediction.progress':
-		case 'channel.prediction.lock':
-		case 'channel.prediction.end':
-
-		// charity
-		case 'channel.charity_campaign.donate':
-		case 'channel.charity_campaign.start':
-		case 'channel.charity_campaign.progress':
-		case 'channel.charity_campaign.stop':
-
-		// goal
-		case 'channel.goal.begin':
-		case 'channel.goal.progress':
-		case 'channel.goal.end':
 	}
 }
 
