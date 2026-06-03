6cc3c3c5a38bcd0ca98ed1dcc5536b14cc00126d
Zaytri <9072119+zaytri@users.noreply.github.com>
2026-06-03 14:03:11 -0700

    choosing account type is now buttons instead of radio

--- src/components/dialog/AuthenticationDialog/AuthTypePage.tsx
+++ src/components/dialog/AuthenticationDialog/AuthTypePage.tsx
@@ -1,92 +1,68 @@
 import { usePage } from '@/contexts/pages/usePage';
 import { usePageContext } from '@/contexts/pages/usePageContext';
-import type { Account } from '@@/json/accounts';
 import BookSvg from '@@/svg/BookSvg';
 import ChatBubbleSvg from '@@/svg/ChatBubbleSvg';
-import CheckSvg from '@@/svg/CheckSvg';
-import {
-	Description,
-	Field,
-	Label,
-	Radio,
-	RadioGroup,
-} from '@headlessui/react';
-import { useState } from 'react';
 import type { AuthenticationContext, AuthenticationPages } from '.';
-import DialogConfirmButton from '../DialogButton/DialogConfirmButton';
+import DialogCancelButton from '../DialogButton/DialogCancelButton';
 
 export default function AuthTypePage() {
 	const { setPage } = usePage<AuthenticationPages>();
 	const { setType } = usePageContext<AuthenticationContext>();
-	const [tempType, setTempType] = useState<Account['type']>();
 
 	return (
 		<div className='flex flex-1 flex-col justify-between gap-6'>
-			<RadioGroup
-				value={tempType}
-				onChange={setTempType}
-				className='flex flex-col gap-4'
-			>
-				<AuthTypeOption
-					value='read'
+			<div className='flex flex-col gap-4'>
+				<AuthTypeButton
 					description='Reads event data from this account such as follows, subscriptions, chat messages, etc.'
-				>
-					<BookSvg className='h-4.5' />
-					<p>Read Account</p>
-				</AuthTypeOption>
-				<AuthTypeOption
-					value='bot'
-					description='Can send chat messages from this account.'
-				>
-					<ChatBubbleSvg className='h-4.5' />
-					<p>Bot Account</p>
-				</AuthTypeOption>
-			</RadioGroup>
-
-			<div className='flex justify-end'>
-				<DialogConfirmButton
-					disabled={tempType === undefined}
 					onClick={() => {
-						// this shouldn't happen since the button is disabled
-						if (tempType === undefined) return;
-
-						setType(tempType);
+						setType('read');
 						setPage('service');
 					}}
 				>
-					Next
-				</DialogConfirmButton>
+					<BookSvg className='h-4.5' />
+					<p>Read Account</p>
+				</AuthTypeButton>
+				<AuthTypeButton
+					description='Can send chat messages from this account.'
+					onClick={() => {
+						setType('bot');
+						setPage('service');
+					}}
+				>
+					<ChatBubbleSvg className='h-4.5' />
+					<p>Bot Account</p>
+				</AuthTypeButton>
+			</div>
+
+			<div className='flex justify-end'>
+				<DialogCancelButton />
 			</div>
 		</div>
 	);
 }
 
-type AuthTypeOptionProps = {
-	value: Account['type'];
+type AuthTypeButtonProps = {
+	onClick: VoidFunction;
 	description: string;
 };
 
-function AuthTypeOption({
-	value,
+function AuthTypeButton({
+	onClick,
 	children,
 	description,
-}: Props.WithChildren<AuthTypeOptionProps>) {
+}: Props.WithChildren<AuthTypeButtonProps>) {
 	return (
-		<Field className='rounded-2 bg-white text-zinc-600 outline-2 -outline-offset-1 outline-zinc-300 has-data-checked:outline-zinc-800 over:bg-lime-200 over:text-green-900 over:outline-4 over:outline-lime-600'>
-			<Radio
-				value={value}
-				className='group/radio flex cursor-pointer flex-col overflow-hidden rounded-2 outline-offset-4 outline-lime-600 focus-visible:outline-4'
-			>
-				<Label className='relative flex cursor-pointer items-center gap-2.5 px-4 pt-2 pb-1.5 font-fredoka text-5 font-medium select-none group-data-checked/radio:bg-zinc-700 group-data-checked/radio:text-white *:group-data-checked/radio:drop-shadow-[0_1px_#0006]'>
+		<button
+			type='button'
+			onClick={onClick}
+			className='rounded-2 bg-white px-4 py-2 text-left text-zinc-600 outline-2 -outline-offset-1 outline-zinc-300 over:bg-lime-200 over:text-green-900 over:outline-4 over:outline-lime-600'
+		>
+			<div className='flex flex-col gap-1 overflow-hidden rounded-2 outline-offset-4 outline-lime-600 focus-visible:outline-4'>
+				<div className='relative flex items-center gap-2.5 font-fredoka text-5 font-medium'>
 					{children}
-					<div className='absolute top-2 right-3 bottom-1.5 hidden items-center group-data-checked/radio:flex'>
-						<CheckSvg className='size-4.5' />
-					</div>
-				</Label>
-				<Description className='cursor-pointer px-4 pt-1 pb-2 text-3.5 font-semibold'>
-					{description}
-				</Description>
-			</Radio>
-		</Field>
+				</div>
+				<div className='text-3.5 font-semibold'>{description}</div>
+			</div>
+		</button>
 	);
 }
