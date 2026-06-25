cef46dc22d1fc7ea129c60fa6ef9c376f1b8e005
Zaytri <9072119+zaytri@users.noreply.github.com>
2026-06-24 23:15:22 -0700

    update pluralmind helper to use null instead of undefined

--- src/helpers/services/pluralmind.ts
+++ src/helpers/services/pluralmind.ts
@@ -1,52 +1,52 @@
-import type { ProxiedMessage, Member, System } from 'pluralmind';
+import type { Member, ProxiedMessage, System } from 'pluralmind';
 import * as pluralmind from 'pluralmind';
 
 import Random from '@/helpers/random';
 import { MOCK_PRONOUNS } from './pronouns';
 
 pluralmind.updateConfig({
-    cacheDuration: 5 * 60 * 1000, // 5 minutes
-})
+	cacheDuration: 5 * 60 * 1000, // 5 minutes
+});
 
 export async function getSystemProxiedMessage(
-    platform: 'twitch',
-    userId: string,
-    message: string,
+	platform: 'twitch',
+	userId: string,
+	message: string,
 ) {
-    if (platform !== 'twitch' || !message) return;
+	if (platform !== 'twitch' || !message) return null;
 
-    if (userId.startsWith('mock_')) {
-        return Random.chance(10) ? makeMockMessage(message) : undefined;
-    }
+	if (userId.startsWith('mock_')) {
+		return Random.chance(10) ? makeMockMessage(message) : null;
+	}
 
-    const system = await pluralmind.getSystem(userId);
-    return pluralmind.getProxiedMessage(system, message);
+	const system = await pluralmind.getSystem(userId);
+	return pluralmind.getProxiedMessage(system, message) ?? null;
 }
 
 function makeMockMessage(message: string): ProxiedMessage {
-    const memberId = Random.integer(1, 1000)
-    const member: Member = {
-        id: memberId,
-        name: `MockMember${memberId}`,
-        proxies: ['t'],
-        case_sensitive: false,
-        color: Random.boolean() ? Random.hexCode() : null,
-        pronouns: Random.boolean() ? Random.item(MOCK_PRONOUNS).join('/') : null,
-    }
+	const memberId = Random.integer(1, 1000);
+	const member: Member = {
+		id: memberId,
+		name: `MockMember${memberId}`,
+		proxies: ['t'],
+		case_sensitive: false,
+		color: Random.boolean() ? Random.hexCode() : null,
+		pronouns: Random.boolean() ? Random.item(MOCK_PRONOUNS).join('/') : null,
+	};
 
-    const system: System = {
-        id: Random.integer(1, 1000),
-        color: null,
-        pronouns: null,
-        autoproxy_member_id: null,
-        members: [member],
-    }
+	const system: System = {
+		id: Random.integer(1, 1000),
+		color: null,
+		pronouns: null,
+		autoproxy_member_id: null,
+		members: [member],
+	};
 
-    return {
-        system,
-        member,
-        color: member.color,
-        pronouns: member.pronouns,
-        body: message,
-    }
+	return {
+		system,
+		member,
+		color: member.color,
+		pronouns: member.pronouns,
+		body: message,
+	};
 }
